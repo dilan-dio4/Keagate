96d90e1415e689d7e9c50a6f75e5d8f9bccea6bd
dilan <dio4@pitt.edu>
2022-07-06 15:31:07 -0400

    reorder build steps

--- packages/scripts/keagate.sh
+++ packages/scripts/keagate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 # OS="$(uname -s)"
-if [ -n "$SUDO_USER" ]; then
-    HOME="$(getent passwd $SUDO_USER | cut -d: -f6)"
-fi
+# if [ -n "$SUDO_USER" ]; then
+#     HOME="$(getent passwd $SUDO_USER | cut -d: -f6)"
+# fi
 
 INSTALL_DIR="$HOME"
 FOLDER_NAME="Keagate"
@@ -70,12 +70,6 @@ start_spinner_eval() {
     trap stop_spinner $(seq 0 15)
 }
 
-# -------------------------------------------------------------------------------- #
-# Stop Spinner                                                                     #
-# -------------------------------------------------------------------------------- #
-# A wrapper for stopping the spinner, this simply kills off the process.           #
-# -------------------------------------------------------------------------------- #
-
 stop_spinner() {
     if [[ "${SPIN_PID}" -gt 0 ]]; then
         kill -9 $SPIN_PID >/dev/null 2>&1
@@ -121,41 +115,41 @@ keagate_debug() {
 }
 
 install_node() {
-    start_spinner "Installing Node and NPM via nvm"
+    # start_spinner "Installing Node and NPM via nvm"
     curl -s -o nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
     chmod +x ./nvm.sh
     export NVM_DIR="$HOME/.nvm"
-    ./nvm.sh >/dev/null 2>&1
+    ./nvm.sh
     rm ./nvm.sh
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-    nvm install 16 >/dev/null 2>&1
-    nvm use 16 >/dev/null 2>&1
-    stop_spinner
+    nvm install 16
+    nvm use 16
+    # stop_spinner
 }
 
 # https://stackoverflow.com/a/42876846
-if [[ "$EUID" = 0 ]]; then
-    keagate_debug "Privilege check: already root"
-else
-    sudo -k # make sure to ask for password on next sudo
-    if sudo true; then
-        keagate_debug "Privilege check: Correct password"
-    else
-        keagate_debug "Privilege check: wrong password"
-        echo "Wrong password. Please try again."
-        exit 1
-    fi
-fi
+# if [[ "$EUID" = 0 ]]; then
+#     keagate_debug "Privilege check: already root"
+# else
+#     sudo -k # make sure to ask for password on next sudo
+#     if sudo true; then
+#         keagate_debug "Privilege check: Correct password"
+#     else
+#         keagate_debug "Privilege check: wrong password"
+#         echo "Wrong password. Please try again."
+#         exit 1
+#     fi
+# fi
 
 if ! keagate_has "docker"; then
     keagate_debug "\`Docker\` command not found. Installing..."
     # Install Docker - used for Mongo and Nginx
     curl -fsSL https://get.docker.com -o get-docker.sh
-    start_spinner "Installing Docker"
-    sudo sh get-docker.sh >/dev/null 2>&1
+    # start_spinner "Installing Docker"
+    sudo sh get-docker.sh
     rm get-docker.sh
-    stop_spinner
+    # stop_spinner
 fi
 
 # Fix permissions issue on certain ports from `docker run`
@@ -166,6 +160,7 @@ if keagate_has "node" && keagate_has "npm"; then
     installed_node_version=$(node --version | cut -c 2-3)
     keagate_debug "Installed node version: $installed_node_version"
     if (("$installed_node_version" < "14")); then
+        echo -e '\0033\0143'
         read -p "Your existing node version ($installed_node_version) is too low for Keagate. Would you like me to automatically upgrade Node and NPM? (You can revert back with \`nvm install $installed_node_version && nvm use $installed_node_version\`) [Y/n] " -n 1 -r
         echo # (optional) move to a new line
         if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -183,42 +178,46 @@ cd $INSTALL_DIR
 
 if [ -d "$FOLDER_NAME" ]; then
     keagate_debug "Found an existing $FOLDER_NAME/. Asking for permission to override..."
+    echo -e '\0033\0143'
     read -p "Folder $FOLDER_NAME/ already exists in $INSTALL_DIR. Would you like me to overwrite this? (This will preserve \`config/local.json\`) [Y/n] " -n 1 -r
     echo # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         keagate_debug "Caching existing local.json to a temporary file..."
-        cp -f $FOLDER_NAME/config/local.json ./local.json >/dev/null 2>&1 || true
+        cp -f $FOLDER_NAME/config/local.json ./local.json || true
         rm -rf $FOLDER_NAME
-        start_spinner "Cloning Keagate repo"
-        git clone $REPO_LOCATION >/dev/null 2>&1
-        cp -f ./local.json $FOLDER_NAME/config/local.json >/dev/null 2>&1 || true
+        # start_spinner "Cloning Keagate repo"
+        git clone $REPO_LOCATION
+        cp -f ./local.json $FOLDER_NAME/config/local.json || true
         rm -f ./local.json || true
     fi
 else
-    start_spinner "Cloning Keagate repo"
-    git clone $REPO_LOCATION >/dev/null 2>&1
+    # start_spinner "Cloning Keagate repo"
+    git clone $REPO_LOCATION
 fi
 
-stop_spinner
+# stop_spinner
 
 cd $FOLDER_NAME
 
-start_spinner "Installing and configuring pnpm"
-npm i -g pnpm >/dev/null 2>&1
+# >/dev/null 2>&1
+
+# start_spinner "Installing and configuring pnpm"
+npm i -g pnpm
 export PNPM_HOME="$HOME/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
-pnpm setup >/dev/null 2>&1
-stop_spinner
+pnpm setup
+# stop_spinner
 
-start_spinner "Installing Keagate depencencies"
+# start_spinner "Installing Keagate depencencies"
 pnpm i --silent -g pm2
 pnpm i --silent
-stop_spinner
+# stop_spinner
 
-start_spinner "Building Keagate"
-pnpm run build >/dev/null 2>&1
-stop_spinner
+# start_spinner "Building Keagate"
+pnpm run build
+# stop_spinner
 
+echo -e '\0033\0143'
 node packages/scripts/build/configure.js $NODE_ARGS
 
-# pnpm run start >/dev/null 2>&1
+# pnpm run start
