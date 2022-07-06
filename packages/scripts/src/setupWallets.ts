87d0cdeaa9a82601073ae19067b42ec38365f891
dilan <dio4@pitt.edu>
2022-07-06 12:14:47 -0400

    Add ELv2 license

--- packages/scripts/src/setupWallets.ts
+++ packages/scripts/src/setupWallets.ts
@@ -1,4 +1,4 @@
-import { MyConfig, availableCoinlibCurrencies, availableNativeCurrencies, AvailableCurrencies } from '@keagate/common';
+import { MyConfig, availableCoinlibCurrencies, availableNativeCurrencies, AvailableCurrencies, currencies } from '@keagate/common';
 import prompts from 'prompts';
 
 export default async function setupWallets(): Promise<Partial<MyConfig>> {
@@ -7,28 +7,33 @@ export default async function setupWallets(): Promise<Partial<MyConfig>> {
         type: "multiselect",
         name: "selectedCurrencies",
         message: "Which currencies would you like to configure. (You can add more later)",
-        choices: [...availableCoinlibCurrencies, ...availableNativeCurrencies].map(ele => ({ title: ele, value: ele })),
+        choices: [...availableCoinlibCurrencies, ...availableNativeCurrencies].map(ele => ({ title: currencies[ele].name, value: ele })),
         hint: '- Space to select. Return to submit'
     })
 
     const keagateConfig: Partial<MyConfig> = {};
 
     for (const currency of selectedCurrencies as AvailableCurrencies[]) {
+        const currencyName = currencies[currency].name;
+
         const { publicKey, privateKey, shouldPrivateKey } = await prompts([
             {
                 type: "text",
                 name: "publicKey",
-                message: `What's your ${currency} public key (where Keagate will send to)?`,
+                message: `What's your ${currencyName} public key (where Keagate will send to)?`,
             },
             {
                 type: "toggle",
                 name: "shouldPrivateKey",
-                message: `Would you like to be able to send ${currency} from your admin wallet via Keagate's API (requires private key)?`
+                message: `Would you like to be able to send ${currencyName} from your admin wallet via Keagate's API (requires private key)?`,
+                initial: false,
+                active: 'yes',
+                inactive: 'no'
             },
             {
                 type: prev => prev ? "text" : null,
                 name: "privateKey",
-                message:`What's your ${currency} private key?`,
+                message:`What's your ${currencyName} private key?`,
             }
         ])
         keagateConfig[currency] = {
