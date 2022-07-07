8870e50019152691de2ff951bafcfef401d79a6b
dilan <dio4@pitt.edu>
2022-07-07 14:07:01 -0400

    Prettier success message

--- packages/scripts/src/configure.ts
+++ packages/scripts/src/configure.ts
@@ -9,6 +9,7 @@ import { existsSync, writeFileSync } from 'fs';
 import path from 'path';
 import opts, { setOpts } from './opts';
 import spawnAsync from '@expo/spawn-async';
+import kleur from 'kleur';
 
 program
     .name('Keagate configure')
@@ -46,19 +47,42 @@ async function main() {
     const prettyConfig = JSON.stringify(config, null, 2);
 
     if (opts().dryrun) {
-        console.log(prettyConfig);
+        logger.log(prettyConfig);
     } else {
         if (existsSync(path.join(__dirname, '..', '..', '..', 'config/', 'local.json'))) {
             logger.log(
-                1,
-                'A `config/local.json` already exists. To preserve the integrity of your previous configuration. This new configuration will be written to `config/local2.json`. Manually the new configuration into `config/local.json`, as needed.',
+                `A ${kleur.italic(`config/local.json`)} already exists. To preserve the integrity ` +
+                `of your previous configuration. This new configuration will be ` +
+                `written to ${kleur.italic(`config/local2.json`)}. ${kleur.bold(`Manually merge`)} ` +
+                `the new configuration into ${kleur.italic(`config/local.json`)} as needed.`
             );
             writeFileSync(path.join(__dirname, '..', '..', '..', 'config/', 'local2.json'), prettyConfig);
         } else {
             writeFileSync(path.join(__dirname, '..', '..', '..', 'config/', 'local.json'), prettyConfig);
         }
-        spawnAsync('pnpm', ['run', '']);
+        await spawnAsync('pm2', ['stop', 'Keagate']);
+        await spawnAsync('pm2', ['del', 'Keagate']);
+        const startSpawn = spawnAsync('pm2', ['start', 'packages/backend/build/index.js', '--name', '"Keagate"', '--time'], { cwd: path.join(__dirname, '..', '..', '..') });
+        startSpawn.child.stderr.on('data', data => logger.error(data))
+        startSpawn.child.stdout.on('data', data => logger.debug(data))
+        await startSpawn;
+        await spawnAsync('pm2', ['save'], { cwd: path.join(__dirname, '..', '..', '..') });
     }
+
+    logger.log(
+        `Keagate is successfully running on this machine. If you're using a cloud provider like ` +
+        `AWS or Azure, please be sure to ${kleur.bold(`enable public access via HTTP(S)`)}. ` +
+        `Then, you can locate your API documentation at ${kleur.underline(config.HOST + '/docs')} ` +
+        `and OpenAPI schema with ${kleur.italic(`curl localhost/docs/yaml`)}. ` +
+        `Most of the API routes require a ${kleur.bold(`KEAGATE_API_KEY`)} header. Your has ` +
+        `been randomly generated as: "${config.KEAGATE_API_KEY}". You can always find and edit ` +
+        `this value [and many others] in ${kleur.italic(`config/local.json`)}. ` +
+        `\n\n ` +
+        `The Keagate server is running via ${kleur.italic(`pm2`)}. To restart the server ` +
+        `execute ${kleur.italic(`pm2 restart Keagate`)}. To monitor the server ` +
+        `execute ${kleur.italic(`pm2 monit Keagate`)}. Read more about ${kleur.italic(`pm2`)} ` +
+        `at ${kleur.underline(`https://pm2.keymetrics.io/`)}.`
+    )
 
     process.exit();
 }
